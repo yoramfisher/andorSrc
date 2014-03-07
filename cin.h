8521adab80c21b3722c2e7218ad7f060b7d7b260
Jaime A Farrington <jfarrington@xf23id1-ioc2.cs.nsls2.local>
2014-03-07 17:51:39 -0500

    Updated power up routine, bugfixes and addition of r/wr fclk functions

--- cin.h
+++ cin.h
@@ -165,7 +165,7 @@ int cin_load_config(struct cin_port* cp,char *filename);
 int cin_load_firmware(struct cin_port* cp,struct cin_port* dcp, char *filename);				
 int cin_set_fclk(struct cin_port* cp,uint16_t clkfreq); 
 /*
- * Input:clkfreq={125, 180, 200, 250}(MHz)
+ * Input:clkfreq={125, 200, 250}(MHz)
  */	//TODO:-Check that clock is properlly set
 
 int cin_get_fclk_status(struct cin_port* cp); 
@@ -205,13 +205,8 @@ uint16_t cin_get_trigger_status (struct cin_port* cp);
 
 int cin_set_trigger_mode(struct cin_port* cp,int val);
 /*
-<<<<<<< HEAD
- * Input:val= {0-Stop Triggers, set Number of exposures = 1, \
- * 1-Continuous Trigger, set Number of exposures = 0}
-=======
  * Input:val = 0 - Single Trigger Mode 
  *       val = 1 - Continuous Trigger Mode 
->>>>>>> 28df65c23cd772f60e44cbec2b90a0d1abcb56c0
  */
 
 int cin_set_exposure_time(struct cin_port* cp,float e_time);  
